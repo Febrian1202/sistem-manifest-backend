d7502f7b27ac7ef8c06b47893988b68bea23ed02
M. Febrian Syah <141829479+Febrian1202@users.noreply.github.com>
2026-06-04 20:49:00 +0800

    feat: add favicon referencing USN logo to both app layouts

--- resources/views/components/auth/layout/app.blade.php
+++ resources/views/components/auth/layout/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - USN Manifest</title>
+    <link rel="icon" href="{{ asset('assets/logo-usn.png') }}" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.5/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - USN Manifest</title>
+    <link rel="icon" href="{{ asset('assets/logo-usn.png') }}" type="image/png">
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
